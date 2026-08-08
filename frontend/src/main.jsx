7e963c23caa91d0fbb98e4829c77aee558597826
aswin <aswinvarma956@gmail.com>
2026-08-08 21:56:20 +0530

    fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import './index.css' 
-const ID = import.meta.env.CLIENT_ID;
+const ID = import.meta.env.VITE_CLIENT_ID;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
